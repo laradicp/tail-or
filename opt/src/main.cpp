7b23bcab9f854d46ae57e884d970ace0028ffe54
Lara <laradicp@gmail.com>
2023-11-23 16:20:11 -0300

    move to opt folder

--- opt/src/main.cpp
+++ opt/src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 {
     if(argc != 2)
     {
-        cout << "Correct usage: ./cap-flp.bin <path>" << endl;
+        cout << "Correct usage: ./opt.bin <path>" << endl;
         exit(1);
     }
 
